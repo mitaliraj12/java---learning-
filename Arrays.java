f40bcb8838efbf9ec7f14a1e1b6d9bc003407889
Mitali Raj <mitaliraj1201@gmail.com>
2026-07-14 13:08:51 +0530

    Added Java Arrays Program

---
+++ Arrays.java
@@ -0,0 +1,10 @@
+public class Arrays {
+    public static void main(String[] args) {
+
+        int marks[] = {70, 80, 90, 85, 95};
+
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}
