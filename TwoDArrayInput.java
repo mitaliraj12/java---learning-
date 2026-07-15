4f2746e324bb5cdc6c74e021512dfe2c858774e8
Mitali Raj <mitaliraj1201@gmail.com>
2026-07-15 23:33:22 +0530

    Two Dimensional Array  Input

---
+++ TwoDArrayInput.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class TwoDArrayInput {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a[][] = new int[2][2];
+
+        System.out.println("Enter 4 Elements:");
+
+        for(int i = 0; i < a.length; i++) {
+            for(int j = 0; j < a[i].length; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Array Elements:");
+
+        for(int i = 0; i < a.length; i++) {
+            for(int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
